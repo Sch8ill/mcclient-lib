f5226d4f7670c64c31772ec45114333e462074cb
Sch8ill <sch8dill@gmail.com>
2022-11-12 16:58:29 +0100

    Create m.py

---
+++ m.py
@@ -0,0 +1,9 @@
+from mcstatus import JavaServer
+
+# You can pass the same address you'd enter into the address field in minecraft into the 'lookup' function
+# If you know the host and port, you may skip this and use JavaServer("example.org", 1234)
+server = JavaServer.lookup("185.14.95.45:29565")
+
+
+q = server.query()
+print(q.raw)
