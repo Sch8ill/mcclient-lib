216c7becf4d0c0a14751c3ace45efd00dc479412
Sch8ill <sch8dill@gmail.com>
2022-11-13 01:24:08 +0100

    Update q.py

--- q.py
+++ q.py
@@ -6,6 +6,7 @@ __author__ = "Sch8ill"
 import socket
 import struct
 import random
+import logging
 
 
 class Packet:
@@ -16,11 +17,10 @@ class Packet:
 
 
     def pack(self):
-        packet = b"\xFE\xFD"
-        packet += struct.pack("B", self.type)
+        packet = b"\xFE\xFD" # every packet starts with this, blame mojang
+        packet += struct.pack("!B", self.type)
         packet +=  struct.pack('>l', self.session_id)
         packet += self.payload
-
         return packet
 
 
@@ -31,34 +31,47 @@ class QueryClient:
         self.host = host
         self.port = port
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.log = logging.getLogger("Queryclient")
         self.sock.settimeout(8)
-        self.session_id = 1 #random.randint(1, 69)##############################################################
 
 
     def _handshake(self):
-        packet_type = 9
+        self.session_id = random.randint(0, 2**31) & 0x0F0F0F0F # generate session id from int between 0 and 2147483648
         packet = Packet(
-            packet_type,
+            9, # packettype 9 for handshaking
             self.session_id,
-            b""
+            b"" # empty payload
         )
         packet = packet.pack()
         self._send(packet)
         res = self._recv()
-        self.token = struct.pack('>l', int(res[2][:-1]))
-        print(self.token)
-        
-        
+        self.token = struct.pack('>l', int(res[2][:-1])) # extract token from response
+        self.log.debug("challenge token: " + str(self.token))
 
+
+    def get_stats(self):
+        self._handshake()
+
+        payload = self.token + b"\x00\x00\x00\x00" # challenge token and four zero bytes for full stat request
+        packet = Packet(
+            0, # packettype 0 for stat resolving
+            self.session_id,
+            payload
+        )
+        packet = packet.pack()
+        self._send(packet)
+        res = self._recv()
+        
+        
 
     def _send(self, packet):
-        print("send: " + str(packet))
+        self.log.debug("send: " + str(packet))
         return self.sock.sendto(packet, (self.host, self.port))
 
 
     def _recv(self):
         res = self.sock.recv(2048)
-        print("received: " + str(res))
+        self.log.debug("received: " + str(res))
         type = res[0]
         session_id = res[1:5]
         payload = res[5:]
@@ -73,5 +86,7 @@ class QueryClient:
 
 
 if __name__ == "__main__":
-    c = QueryClient()
-    c._handshake()
+    logging.basicConfig(format="[%(asctime)s][%(name)s][%(levelname)s]: %(message)s", level=logging.NOTSET)
+
+    c = QueryClient(host="185.14.95.45", port=29565)
+    c.get_stats()
