0c43e8d57827fc9e92f3d3cf92eb9f3b42e99583
Sch8ill <sch8dill@gmail.com>
2022-11-14 14:25:30 +0100

    Update MCClient.py

--- MCClient.py
+++ MCClient.py
@@ -32,3 +32,9 @@ if __name__ == "__main__":
 
     print(slp)
     print(q)
+
+
+
+
+    # {'previewsChat': False, 'enforcesSecureChat': False, 'description': {'extra': [{'text': 'Ich verkaufe meinen Server, ganz ganz billig, heute Nacht!'}], 'text': ''}, 'players': {'max': 69, 'online': 1, 'sample': [{'id': 'd40e6959-872b-47ae-96f6-0377451fd61e', 'name': 'Sch8ill'}]}, 'version': {'name': 'Spigot 1.19.2', 'protocol': 760}}
+    # {'motd': 'Ich verkaufe meinen Server, ganz ganz billig, heute Nacht!', 'gametype': 'SMP', 'game_id': 'MINECRAFT', 'version': '1.19.2', 'plugins': [], 'map': 'world', 'numplayers': 1, 'maxplayers': 69, 'hostport': 29565, 'hostip': '172.18.0.2', 'software': 'CraftBukkit on Bukkit 1.19.2-R0.1-SNAPSHOT', 'players': ['Sch8ill']}
