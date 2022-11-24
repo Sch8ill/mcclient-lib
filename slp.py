a61a2f01987754ee6733485fa694f2bf3dcb66d1
Sch8ill <92382209+Sch8ill@users.noreply.github.com>
2022-11-24 18:00:54 +0100

    Update slp.py

--- slp.py
+++ slp.py
@@ -22,21 +22,6 @@ class VarInt: # class to pack and unpack Varints
         return ordinal
 
 
-    @staticmethod
-    def unpack(sock):
-        data = 0
-        for i in range(5):
-            ordinal = sock.recv(1)
-            if len(ordinal) == 0:
-                break
-
-            byte = ord(ordinal)
-            data |= (byte & 0x7F) << 7*i
-            if not byte & 0x80:
-                break
-        return data
-
-
 
 class Packet:
     def __init__(self, id, fields=[]):
