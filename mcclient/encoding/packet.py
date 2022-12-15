69ae1185e9fdba17fb1b8141c74a512a35e2b139
Sch8ill <92382209+Sch8ill@users.noreply.github.com>
2022-12-15 13:26:22 +0000

    v

---
+++ mcclient/encoding/packet.py
@@ -0,0 +1,36 @@
+
+import struct
+
+
+class Packet:
+    def __init__(self, fields=[]):
+        self.fields = fields
+        self.varint = VarInt()
+
+
+    def pack(self):
+        packet = b""
+        for field in self.fields:
+            field = self._encode(field)
+            packet += field
+
+        packet = self.varint.pack(len(packet)) + packet # add packetlength
+        return packet
+
+
+    def _encode(self, data):
+        if type(data) == int:
+            data = struct.pack(">H", data)
+
+        elif type(data) == str:
+            data = data.encode("utf-8")
+            data = self.varint.pack(len(data)) + data
+
+        elif type(data) == bool:
+            data = b"\x01" if data else b"\x00"
+
+        elif type(data) == float:
+            print(data)
+            data = struct.pack(">Q", int(data))
+            print(data)
+        return data
