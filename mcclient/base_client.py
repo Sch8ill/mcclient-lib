0928f378a02c293968dedf7ee8730794400e51ed
Sch8ill <sch8dill@gmail.com>
2022-12-15 18:00:10 +0100

    Update base_client.py

--- mcclient/base_client.py
+++ mcclient/base_client.py
@@ -1,11 +1,36 @@
 
 __author__ = "Sch8ill"
 
-
+import struct
 import socket
-from mcclient.encoding.varint import VarInt
-from mcclient.utils import VarInt
 from mcclient.encoding.packet import Packet
+from mcclient.encoding.varint import VarInt
+
+class VarInt: # class to pack and unpack Varints
+    @staticmethod
+    def pack(data):
+        ordinal = b''
+        while data != 0:
+            byte = data & 0x7F
+            data >>= 7
+            ordinal += struct.pack('B', byte | (0x80 if data > 0 else 0))
+        return ordinal
+
+
+    @staticmethod
+    def unpack(sock):
+        data = 0
+        for i in range(5):
+            ordinal = sock.recv(1)
+            if len(ordinal) == 0:
+                break
+
+            byte = ord(ordinal)
+            data |= (byte & 0x7F) << 7*i
+            if not byte & 0x80:
+                break
+
+        return data
 
 
 
