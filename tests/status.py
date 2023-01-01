458f55aab433673d5746f176e2c3aa9568d5cd7e
Sch8ill <sch8dill@gmail.com>
2023-01-01 17:23:54 +0100

    Update status.py

--- tests/status.py
+++ tests/status.py
@@ -14,7 +14,7 @@ query_client = QueryClient(host="mc.lpmitkev.de", port=25565)
 
 bedrock_client = BedrockSLPClient("mcprison.com")
 
-print("bedrock: " + str(bedrock_client.get_status().res))
+#print("bedrock: " + str(bedrock_client.get_status().res))
 
 print("slp: " + str(slp_client.get_status().res))
 
