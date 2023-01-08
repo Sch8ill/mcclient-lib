fa0468473ab5b58ad8061569c22b83d29e4eb904
Sch8ill <sch8dill@gmail.com>
2023-01-08 01:47:17 +0100

    added SRV record tests

--- tests/status.py
+++ tests/status.py
@@ -6,15 +6,15 @@ from mcclient import BedrockSLPClient
 
 
 
-slp_client = SLPClient(host="mc.internetpolice.ga", port=29565) # random servers
+slp_client = SLPClient(host="mc.internetpolice.ga") # random server, SRV record test
 
-legacy_slp_client = LegacySLPClient(host="mc.internetpolice.ga", port=29565)
+legacy_slp_client = LegacySLPClient(host="mc.internetpolice.ga") # SRV record test
 
 query_client = QueryClient(host="mc.lpmitkev.de", port=25565)
 
-bedrock_client = BedrockSLPClient("mcprison.com")
+bedrock_client = BedrockSLPClient("geo.hivebedrock.network")
 
-#print("bedrock: " + str(bedrock_client.get_status().res))
+print("bedrock: " + str(bedrock_client.get_status().res))
 
 print("slp: " + str(slp_client.get_status().res))
 
