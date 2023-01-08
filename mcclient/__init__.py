8110a371684f3f4b0713330681b0216ebf0453d8
Sch8ill <sch8dill@gmail.com>
2023-01-08 17:00:39 +0100

    Update __init__.py

--- mcclient/__init__.py
+++ mcclient/__init__.py
@@ -1,5 +1,5 @@
-from mcclient.base_client import BaseClient
 from mcclient.slp import SLPClient
 from mcclient.slp import LegacySLPClient
+from mcclient.base_client import BaseClient
 from mcclient.bedrock_slp import BedrockSLPClient
 from mcclient.query.query_client import QueryClient
