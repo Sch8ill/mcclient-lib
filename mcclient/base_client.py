fae20ae530d4d41082bad88e158805303b677d0c
Sch8ill <sch8dill@gmail.com>
2023-01-18 20:14:21 +0100

    fix bug/type

--- mcclient/base_client.py
+++ mcclient/base_client.py
@@ -9,7 +9,7 @@ class BaseClient:
     def __init__(self, host="localhost", port=25565, timeout=5, version=47, srv=True):
         self.get_host(host, port, srv=srv)
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.sock.setsocketimeout(timeout)
+        self.sock.settimeout(timeout)
         self.varint = VarInt()
         self.connected = False
         self.protocoll_version = self.varint.pack(version)
