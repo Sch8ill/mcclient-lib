b56634434920ff4d7856cd14ede8fe9425b1aecf
Sch8ill <92382209+Sch8ill@users.noreply.github.com>
2024-06-10 15:33:09 +0200

    improve example

--- examples/status.py
+++ examples/status.py
@@ -1,8 +1,19 @@
 # sample script to get a servers status using SLP
 from mcclient import SLPClient
 
-client = SLPClient("<YOUR_SERVER_ADDRESS>")
+client = SLPClient("hypixel.net")
 res = client.get_status()
 
-for key in res.res:
-    print(f"{key}: {res.res[key]}")
+print(f"host: {res.host}")
+print(f"port: {res.port}")
+print(f"motd: {res.motd}")
+
+print(f"online players: {res.players.online}")
+print(f"max players: {res.players.max}")
+print(f"players list: {res.players.list}")
+
+print(f"version: {res.version.name}")
+print(f"protocol version: {res.version.protocol}")
+
+print(f"favicon: {res.favicon is not None}")
+print(f"timestamp: {res.timestamp}")
